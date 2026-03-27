b77c8a59d03568ef3aad7c06dd32060c916361b8
Ditherys <reyesditherb@gmail.com>
2026-03-27 21:19:25 +0800

    Update supabase.config.js

--- supabase.config.js
+++ supabase.config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "https://fdfcllfajqeozlokvala.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_bkoXzA0GqhF91O-UIhAd5Q_0Fn3tGI5";
+export const SUPABASE_URL = "https://pnleinprsvaijfkvxfyi.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_eaj3dAdz6t5e1FOjT5ySZA_jwVNt3_c";
 export const HISTORY_DAYS = 2;
